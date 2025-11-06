aed3cf57dbd86bd1db438621da99a4771330163e
Alex Cheung <alexjakhangcheung@gmail.com>
2025-11-06 13:42:19 -0500

    Added ollama including starter code

--- sample_llm_project/main.py
+++ sample_llm_project/main.py
@@ -1,5 +1,17 @@
+from ollama import chat
+from ollama import ChatResponse
+
+
 def main():
-    print("Hello from sample-llm-project!")
+    response: ChatResponse = chat(model='gemma3:1b', messages=[
+    {
+        'role': 'user',
+        'content': 'Why is the sky blue?',
+    },
+    ])
+    print(response['message']['content'])
+    # or access fields directly from the response object
+    print(response.message.content)
 
 
 if __name__ == "__main__":
